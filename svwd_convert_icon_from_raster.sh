0de0a041e6313e34d27aa3c22802f579fa1eb76c
Andy Townsend <ajtown@dell5000.atownsend.org.uk>
2024-03-02 20:31:49 +0000

    Updated copyright to 2024

--- svwd_convert_icon_from_raster.sh
+++ svwd_convert_icon_from_raster.sh
@@ -2,7 +2,7 @@
 # -----------------------------------------------------------------------------
 # svwd_convert_icon_from_raster.sh
 #
-# Copyright (C) 2023  Andy Townsend
+# Copyright (C) 2023-2024  Andy Townsend
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
--- svwd_create_sprite.sh
+++ svwd_create_sprite.sh
@@ -2,7 +2,7 @@
 # -----------------------------------------------------------------------------
 # svwd_create_sprite.sh
 #
-# Copyright (C) 2023  Andy Townsend
+# Copyright (C) 2023-2024  Andy Townsend
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by

59c8b51131fa82286ecd53cd7bec1b925a2790ad
Andy Townsend <ajtown@dell5000.atownsend.org.uk>
2025-01-12 21:31:30 +0000

    Made the rock and tidal rock patterns sharper.

--- resources/svwd01_call_icon_convert.sh
+++ resources/svwd01_call_icon_convert.sh
@@ -937,8 +937,6 @@
 ~/src/SomeoneElse-vector-web-display/svwd_convert_icon_from_raster.sh ~/src/SomeoneElse-vector-web-display/resources/svwd01_source_icons/meadowperpetual32.png    ~/src/SomeoneElse-vector-web-display/resources/svwd01_icons/pattern_landuse_meadowperpetual.png
 ~/src/SomeoneElse-vector-web-display/svwd_convert_icon_from_raster.sh ~/src/SomeoneElse-vector-web-display/resources/svwd01_source_icons/saltmarsh32.png          ~/src/SomeoneElse-vector-web-display/resources/svwd01_icons/pattern_landuse_saltmarsh.png
 ~/src/SomeoneElse-vector-web-display/svwd_convert_icon_from_raster.sh ~/src/SomeoneElse-vector-web-display/resources/svwd01_source_icons/reedbed32.png            ~/src/SomeoneElse-vector-web-display/resources/svwd01_icons/pattern_landuse_reedbed.png
-~/src/SomeoneElse-vector-web-display/svwd_convert_icon_from_raster.sh ~/src/SomeoneElse-vector-web-display/resources/svwd01_source_icons/rock32.png             ~/src/SomeoneElse-vector-web-display/resources/svwd01_icons/pattern_natural_rock.png
-~/src/SomeoneElse-vector-web-display/svwd_convert_icon_from_raster.sh ~/src/SomeoneElse-vector-web-display/resources/svwd01_source_icons/tidal_rock32.png       ~/src/SomeoneElse-vector-web-display/resources/svwd01_icons/pattern_natural_tidal_rock.png
 ~/src/SomeoneElse-vector-web-display/svwd_convert_icon_from_raster.sh ~/src/SomeoneElse-vector-web-display/resources/svwd01_source_icons/tidal_sand32.png       ~/src/SomeoneElse-vector-web-display/resources/svwd01_icons/pattern_natural_tidal_sand.png
 ~/src/SomeoneElse-vector-web-display/svwd_convert_icon_from_raster.sh ~/src/SomeoneElse-vector-web-display/resources/svwd01_source_icons/scree32.png            ~/src/SomeoneElse-vector-web-display/resources/svwd01_icons/pattern_natural_scree.png
 ~/src/SomeoneElse-vector-web-display/svwd_convert_icon_from_raster.sh ~/src/SomeoneElse-vector-web-display/resources/svwd01_source_icons/tidal_scree32.png      ~/src/SomeoneElse-vector-web-display/resources/svwd01_icons/pattern_natural_tidal_scree.png
@@ -968,6 +966,8 @@ cp ~/src/SomeoneElse-vector-web-display/resources/svwd01_source_icons/beach32.pn
 cp ~/src/SomeoneElse-vector-web-display/resources/svwd01_source_icons/tidal_beach32.png        ~/src/SomeoneElse-vector-web-display/resources/svwd01_icons/pattern_natural_tidal_beach.png
 cp ~/src/SomeoneElse-vector-web-display/resources/svwd01_source_icons/mud32.png                ~/src/SomeoneElse-vector-web-display/resources/svwd01_icons/pattern_natural_mud.png
 cp ~/src/SomeoneElse-vector-web-display/resources/svwd01_source_icons/tidal_mud32.png          ~/src/SomeoneElse-vector-web-display/resources/svwd01_icons/pattern_natural_tidal_mud.png
+cp ~/src/SomeoneElse-vector-web-display/resources/svwd01_source_icons/rock32.png               ~/src/SomeoneElse-vector-web-display/resources/svwd01_icons/pattern_natural_rock.png
+cp ~/src/SomeoneElse-vector-web-display/resources/svwd01_source_icons/tidal_rock32.png         ~/src/SomeoneElse-vector-web-display/resources/svwd01_icons/pattern_natural_tidal_rock.png
 
 #
 # Some area patterns copy from a local 64x64 source
